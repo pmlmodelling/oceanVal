e3e056331564e5c7abe35e5541aa820b3f4798be
Robert  Wilson <robertwilson190@gmail.com>
2025-11-25 10:32:12 +0000

    tweak conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,15 +33,11 @@ extensions = [
     "sphinx.ext.extlinks",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
-    "numpydoc",
-    "sphinx_rtd_theme",
-    "nbsphinx",
     'sphinx.ext.todo',
     'sphinx.ext.githubpages',
 ]
 
-html_theme = "sphinx_rtd_theme"
-#html_theme = "haiku"
+html_theme = "haiku"
 #html_theme = "nature"
 
 
@@ -72,7 +68,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
